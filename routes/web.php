e10a20fa8f74e605a6bce4751ccc6eff13c44e02
gs shohel1314 <gsshohel1314@gmail.com>
2021-03-14 11:51:31 +0600

    setup layouts, controller, middleware, route group, logout, prevent back etc

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['as'=>'app.', 'prefix'=>'app', 'namespace'=>'Backend', 'middleware'=>['auth','preventBack']], function(){
+    Route::get('/dashboard', 'DashboardController')->name('dashboard');
+});
